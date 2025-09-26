b351ca58d8bcc26dd2f8ccb51c0f825e8d7bd169
kumarlalait79 <luckyarya0101@gmail.com>
2025-09-26 20:44:41 +0530

    subset

--- 28_RecursionSubsetsSubseq/1_Subset.cpp
+++ 28_RecursionSubsetsSubseq/1_Subset.cpp
@@ -4,8 +4,38 @@ using namespace std;
 
 class Solution {
 
+/*
+    variable ke kaam
+
+1 - vector<int> nums : 
+    Purpose: Ye tumhara input array hai jisme se subsets generate karne hai.
+    Example: {1, 2, 3}
+    Kaam: Ye array decide karega ki kaunse elements subset mai aa sakte hai.
+
+2 - vector<int> output
+    Purpose: Ye temporary vector hai jo current subset ko hold karta hai.
+    Example: Agar recursion mai {1,3} subset ban raha hai, to output = {1,3}
+    Kaam: Jab recursion index ke end tak pahuchta hai, ye subset ans mai push kar diya jata hai.
+
+3 - int index
+    Purpose: Ye current position batata hai nums array me.
+    Kaam: Har recursion call mai decide karta hai ki kaunsa element include/exclude karna hai.
+    Example:
+    index = 0 → element = 1
+    index = 1 → element = 2
+    index = 2 → element = 3
+
+4. vector<vector<int>> &ans
+    Naam: ans
+    Purpose: Ye final result hai jisme saare generated subsets store hote hai.
+    Type: vector of vector → har subset ek vector, aur sab subsets ek 2D vector me stored.
+    Why &: & lagaya hai taaki reference pass ho aur recursion me ye copy na ho, direct same memory mai update ho jaye.
+    Kaam: Jab recursion ke base case me index = nums.size() ho jata hai, tab current subset output ko ans mai add kar dete hai.
+*/
+
 private:
-    void solveFn(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans) { // &ans likha hai dhyan dena warna copy pass ho jayega, yaha se ref pass kr re
+    void solveFn(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans) { // &ans likha hai dhyan dena warna copy pass ho jayega, yaha se ref pass kr re
+        
         // base case
         if (index >= nums.size()) {
             ans.push_back(output);
