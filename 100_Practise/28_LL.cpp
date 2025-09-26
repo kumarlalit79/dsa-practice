d9c93509518916efa0d6ec45e3633cf9a24872c2
kumarlalait79 <luckyarya0101@gmail.com>
2025-09-26 22:39:48 +0530

    LL

--- 100_Practise/28_LL.cpp
+++ 100_Practise/28_LL.cpp
@@ -12,6 +12,16 @@ class Node{
         this->next = NULL;
     }
 
+    ~Node(){
+        int value = this->data;
+        if (this -> next !=NULL)
+        {
+            delete next;
+            this -> next = NULL;
+        }
+        cout << "Memory is free for : " << value << endl;
+    }
+
 };
 
 void insertAtHead(Node* &head , int data){
@@ -52,7 +62,35 @@ void insertAtPosition(Node* &head, Node* &tail, int position, int data){
     {
         insertAtTail(tail, data);
     }
-    
+}
+
+void deleteLL(Node* head, int position){
+
+    if (position == 1)
+    {
+        Node* temp = head;
+        head = head -> next;
+        temp -> next = NULL;
+
+        delete temp;
+    }
+    else{
+
+        Node* curr = head;
+        Node* prev = NULL;
+        int count = 1;
+        while (count < position)
+        {
+            prev = curr;
+            curr = curr -> next;
+            count++;
+        }
+        
+        prev -> next = curr -> next;
+        curr -> next = NULL;
+        delete curr;
+
+    }
 
 }
 
@@ -74,12 +112,20 @@ int main(){
 
     insertAtTail(tail,11);
     insertAtTail(tail,13);
+    insertAtTail(tail,14);
+    insertAtTail(tail,15);
+    insertAtTail(tail,16);
     print(head);
 
-    int position = 1;
+    int position;
+    cout << "Enter position" << endl;
+    cin >> position;
 
-    insertAtPosition(head, tail, position , 9);
+    //insertAtPosition(head, tail, position , 9);
+    //print(head);
+
+    deleteLL(head, position);
     print(head);
-
+    
     return 0;
 }
