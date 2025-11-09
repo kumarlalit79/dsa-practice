28a174fbbb4c2eee0802edc49e6beee26cf09c4d
kumarlalait79 <luckyarya0101@gmail.com>
2025-11-09 10:25:51 +0530

    Pair sum

---
+++ 50_Arrays_Again/9_PairSum.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> pairSum(vector<int> &arr , int s){
+    vector<vector<int>> ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for(int j = i+1; j<arr.size(); j++)
+        {
+            if (arr[i] + arr[j] == s)
+            {
+                vector<int> temp;
+                temp.push_back(min(arr[i] , arr[j]));
+                temp.push_back(max(arr[i] , arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    // last mai sort krdo
+    sort(ans.begin() , ans.end());
+    return ans;
+}
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,5,6};
+    int s = 5;
+    vector<vector<int>> result =  pairSum(arr , s);
+    cout << "Pairs with sum " << s << ": " << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i][0] << ", " << result[i][1] << endl;
+    }
+
+ 
+    return 0;
+}
