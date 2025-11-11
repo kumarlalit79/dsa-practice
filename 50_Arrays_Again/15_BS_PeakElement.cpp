78495d2a48b33ab6de4cf072f393ba2c97e556ee
kumarlalait79 <luckyarya0101@gmail.com>
2025-11-11 11:34:55 +0530

    peak element

---
+++ 50_Arrays_Again/15_BS_PeakElement.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int peakElement(int arr[] , int size){
+
+    int start = 0;
+    int end = size - 1;
+
+    int mid = start + (end-start) / 2;
+
+    while (start < end)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+        mid = start + (end-start) / 2;
+    }
+    return start;
+}
+
+int main(){
+
+    int arr[4] = {3,4,5,1};
+    cout << "Index of peak element is : " << peakElement(arr , 4);
+        
+    return 0;
+}
