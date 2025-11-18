1e23375045ec01af6382e7e9e78a509dcf3542b6
kumarlalait79 <luckyarya0101@gmail.com>
2025-11-18 07:16:44 +0530

    Length of string

---
+++ 51_Strings_Again/1_StringLength.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int getLength(char name[]){
+    int count = 0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    
+    char name[20];
+    cout << "Enter name : " ;
+    cin >> name;
+    cout << "Length of string is : " << getLength(name);
+    return 0;
+}
