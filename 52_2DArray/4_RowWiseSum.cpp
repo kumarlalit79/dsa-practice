41bfe689d4e88fe947cebe0b354956be88300364
kumarlalait79 <luckyarya0101@gmail.com>
2025-11-23 10:25:38 +0530

    Row wise sum

---
+++ 52_2DArray/4_RowWiseSum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+void printSum(int arr[][4] , int row , int col){
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << sum << " "; 
+    }   
+}
+
+int main(){
+
+    int arr[3][4];
+    cout << "Enter the elements in the 2D array : " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+        
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    printSum(arr,3,4);
+
+    return 0;
+}
