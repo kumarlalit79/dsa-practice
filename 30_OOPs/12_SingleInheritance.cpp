2fdea04e74f4ca6244dca848a14f88ad981df78d
kumarlalait79 <luckyarya0101@gmail.com>
2025-11-29 22:20:15 +0530

    single inheritance

---
+++ 30_OOPs/12_SingleInheritance.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public : 
+    int age;
+    int weight;
+
+    public :
+    void speak() {
+        cout << "Speaking" << endl;
+    }
+};
+
+class Dog : public Animal{
+
+    // bina kuch yaha pe likhe bhi chal jayega
+
+};
+
+int main(){
+
+    Dog d;
+    d.speak();
+
+    return 0;
+}
